a31676fcac49b70f31eaa8534f85ca7888b94381
saRyanMoreau <ryan@ryanmoreau.ca>
2023-09-26 17:35:28 -0400

    chore(portfolio): railway <> astro woes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
+  site: "https://ryanmoreau.ca",
+  integrations: [mdx(), sitemap(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
